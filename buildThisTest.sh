7bc284d803239f514e58dd8ca8ce58fd6d073238
bnewey <bnewey@raineyelectronics.com>
2020-05-05 13:05:56 -0500

    removed some things

--- buildThisTest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-g++ -g /home/bbnewey/visual-workspace/AC_HEAT/com_redox/src/tests.cpp -Wall  -o /home/bbnewey/visual-workspace/AC_HEAT/com_redox/bin/tests -pthread -ljsoncpp -lmysqlclient -lboost_filesystem -I /usr/include/mysql -L/home/bbnewey/visual-workspace/AC_HEAT/redox/build 
